f91f06d22a3e69f1ca305ca2c7402c6082da2cbf
mlfryman <melanie@frymanet.com>
2014-07-14 20:52:16 -0500

    final

---
+++ app.js
@@ -0,0 +1,107 @@
+// this is a single line comment
+//
+
+/* this 
+ * is
+ * a
+ * awesome!
+ * multiline
+ * comment
+ * */
+
+var a = 3;
+var b = 2;
+var c = a + b;
+
+console.log('This is a string');
+console.log(a);
+console.log(b);
+console.log(c);
+
+var first = 'sally';
+var last = 'smith';
+var fullName = first + ' ' + last;
+
+console.log(first);
+console.log(last);
+console.log(fullName);
+
+var d = Math.pow(2,8);
+
+console.log('2^8 ===', d);
+
+a = 3;
+b = 4; 
+c = 5;
+d = 6;
+
+var base = ((a+b)/(b-c)) * ((b-a)/(c-d));
+var exp = b + a;
+var e = Math.pow(base, exp);
+console.log(e);
+
+// branching with if/else statements
+
+var age = 1;
+
+if(age >= 21){
+  console.log('you are legal to drink');
+}else{
+  console.log('you are NOT legal to drink');
+}
+
+// this is the maximum allowed rating you can see
+
+if(age >= 0 && age <=5){
+  console.log('G rated movie');
+}else if(age <= 12){
+  console.log('PG rated movie');
+}else if(age <= 16){
+  console.log('PG-13 rated movie');
+}else if(age === 17){
+  console.log('R rated movie');
+}else if(age === 18){
+  console.log('NC-17 rated movie');
+}else{
+  console.log('X-rated movie');
+}
+
+// this is a switch statement
+
+var color = 'pink';
+switch(color){
+  case 'green':
+    console.log('I see green');
+    break;
+  case 'blue':
+    console.log('I see blue');
+    break;
+  case 'orange':
+    console.log('I see orange');
+    break;
+  case 'pink':
+    console.log('I see pink');
+    break;
+  case 'black':
+    console.log('I see black');
+    break;
+  default:
+    console.log('That is not a valid color');
+}
+
+// while loop
+
+a = 1;
+
+while(a < 10){
+  console.log('a is looping...', a);
+  //a++;
+  //a = a + 1;
+  a += 2;
+}
+
+//for loop
+
+for(var i = 0; i < 10; i++){
+  console.log('i ===', i);
+}
